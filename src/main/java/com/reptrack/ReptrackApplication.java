72f711e4e53a295d6c39df649429eaa5d234aa68
Ansh Dalsania <ansh6207@gmail.com>
2026-07-13 15:31:37 -0400

    Initial Spring Boot skeleton with working ping endpoint

---
+++ src/main/java/com/reptrack/ReptrackApplication.java
@@ -0,0 +1,13 @@
+package com.reptrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReptrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReptrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reptrack/ReptrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reptrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReptrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
